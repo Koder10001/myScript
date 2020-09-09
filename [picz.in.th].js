ffad659d8fdbaced4c3ae21852aade28c6cbdb2a
Koder10001 <36158661+Koder10001@users.noreply.github.com>
2020-09-08 21:10:16 -0500

    Update [picz.in.th].js

--- [picz.in.th].js
+++ [picz.in.th].js
@@ -1,18 +1,8 @@
-function downloadAll(i,links){
-    if(i < imgs.length){
-        let node = document.createElement("a");
-        node.setAttribute("href",links[i]);
-        node.setAttribute("download","");
-        document.body.appendChild(node)
-        node.click();
-        document.body.removeChild(node)
-        console.log(`${i+1}/${links.length}`);
-        setTimeout(()=>{downloadAll(i+1,links)},500);
-    }
-}
 let imgs = document.querySelectorAll("div.list-item-image.fixed-size > a > img");
 let arr = [];
 for(let node of imgs){
-    arr.push(node.src.replace(".md",""));
+    let url = node.src.replace(".md","");
+    chrome.runtime.sendMessage({download: url}, function(response) {
+        console.log(response.status);
+    });
 }
-downloadAll(0,arr)

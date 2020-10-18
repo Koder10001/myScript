3e55cfcbb98f306ff22c977669ab237a59434503
koder10001 <koder10001@gmail.com>
2020-10-18 16:51:06 -0500

    a

--- [hentaivn.net].js
+++ [hentaivn.net].js
@@ -2,7 +2,5 @@ let imgs = document.querySelectorAll("#image > img")
 let arr = [];
 for(let node of imgs){
     let url = node.src;
-    chrome.runtime.sendMessage({download: url}, function(response) {
-        
-    });
+    console.log(url);
 }

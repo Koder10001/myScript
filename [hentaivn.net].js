f33b5b06d2b190bc74530e177b675284d948f96f
koder10001 <koder10001@gmail.com>
2020-10-18 16:54:52 -0500

    aaa

--- [hentaivn.net].js
+++ [hentaivn.net].js
@@ -1,6 +1,18 @@
+function downloadAll(i,links){
+    if(i < imgs.length){
+        let node = document.createElement("a");
+        node.setAttribute("href",links[i]);
+        node.setAttribute("download","");
+        document.body.appendChild(node)
+        node.click();
+        document.body.removeChild(node)
+        console.log(`${i+1}/${links.length}`);
+        setTimeout(()=>{downloadAll(i+1,links)},500);
+    }
+}
 let imgs = document.querySelectorAll("#image > img")
 let arr = [];
 for(let node of imgs){
-    let url = node.src;
-    console.log(url);
+    arr.push(node.src)
 }
+downloadAll(0,arr)

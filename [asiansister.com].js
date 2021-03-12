8f4fa26fd72b9c052dcdc4bf637b0522bcedf67c
koder10001 <koder10001@gmail.com>
2021-03-12 01:03:04 -0600

    add script

--- [asiansister.com].js
+++ [asiansister.com].js
@@ -9,6 +9,10 @@ function downloadAll(i,links){
         console.log(`${i+1}/${links.length}`);
         setTimeout(()=>{downloadAll(i+1,links)},500);
     }
+    // fetch(links, { "headers": { "accept": "image/avif,image/webp,image/apng,image/*,*/*;q=0.8", "accept-language": "en-US,en;q=0.9", "cache-control": "no-cache", "pragma": "no-cache", "sec-fetch-dest": "image", "sec-fetch-mode": "no-cors", "sec-fetch-site": "cross-site" }, "referrer": "https://hentaivn.net/", "referrerPolicy": "strict-origin-when-cross-origin", "body": null, "method": "GET", "mode": "cors", "credentials": "omit" })
+    // .then(response => {
+    //     console.log(response);
+    // })
 
 }
 imgs = document.querySelectorAll(".rootContant > img")
---
+++ hoyolab.com.js
@@ -0,0 +1,18 @@
+function countConstellation(i)
+{
+    if (i < parseInt(document.querySelectorAll(".value")[4].innerText))
+    {
+        let countLock = document.querySelectorAll(".lock").length;
+        let unlocked = 6 - countLock;
+        console.log(`${document.querySelector(".name").innerText} : Constellation ${unlocked}`);
+        document.querySelectorAll(".right")[1].click()
+        setTimeout(()=>{ countConstellation(++i) },500);
+    }
+}
+
+if(window.location.hostname != "webstatic-sea.hoyolab.com"){
+    window.location.href = document.querySelector("iframe").src;
+}
+else {
+    countConstellation(0)
+}

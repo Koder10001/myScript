8cb17f590bf26a3193b5d122842410dd25810565
koder10001 <koder10001@gmail.com>
2021-03-12 01:18:05 -0600

    aaaaa

--- hoyolab.com.js
+++ hoyolab.com.js
@@ -2,9 +2,14 @@ function countConstellation(i)
 {
     if (i < parseInt(document.querySelectorAll(".value")[4].innerText))
     {
+
+        let charName = document.querySelector(".name").innerText;
+        let charFav = document.querySelector(".fav").innerText;
+        charFav = (charFav)?charFav:"";
+
         let countLock = document.querySelectorAll(".lock").length;
         let unlocked = 6 - countLock;
-        console.log(`${document.querySelector(".name").innerText} : Constellation ${unlocked} Favorite level: ${document.querySelector(".fav").innerText}`);
+        console.log(`${charName} : Constellation ${unlocked} Favorite level: ${charFav}`);
         document.querySelectorAll(".right")[1].click()
         setTimeout(()=>{ countConstellation(++i) },500);
     }
